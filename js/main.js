baf15ac490b1c69cad8eab1c815e1cfb0edaa085
Baxtiyorr <hamrokulovbahtier46@gmail.com>
2023-11-04 10:01:01 +0500

    rest countries search and pagination

--- js/main.js
+++ js/main.js
@@ -1,73 +1,213 @@
-// const form = document.querySelector('.form'),
-//     input = document.getElementById('input_distance'),
-//     onFoot = 3.6,
-//     onBike = 20.1,
-//     inCar = 70,
-//     onPlane = 800,
-//     h2 = document.querySelectorAll('.h2')
+const box = document.querySelector('.card_container')
+const input = document.querySelector('.search_input')
+const loading = document.querySelector('.loading')
+const form = document.querySelector('.form')
+const notFoundMassage = document.querySelector('.not_found')
+const countryData = fetch('https://restcountries.com/v3.1/all')
+const itemsPerpage = 20;
+let currentPage = 1;
+let totalItems = 0;
 
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     const userInp = input.value.trim();
 
-//     const countOnFoot = userInp / onFoot;
-//     const countOnBike = userInp / onBike;
-//     const countInCar = userInp / inCar;
-//     const countOnPlane = userInp / onPlane;
+let newData = [];
 
-//     h2.forEach((element, i) => {
+countryData.then((response) => response.json()).then((countries) => {
+    newData = countries
+    totalItems = newData.length
+    generatePaninationLinks()
+    callCountry('')
+})
 
-//         switch (i) {
-//             case 0:
-//                 element.textContent = `${countOnFoot.toFixed(1)} hours`
-//                 break;
-//             case 1:
-//                 element.textContent = `${countOnBike.toFixed(1)} hours`
-//                 break;
-//             case 2:
-//                 element.textContent = `${countInCar.toFixed(1)} hours`
-//                 break;
-//             case 3:
-//                 element.textContent = `${countOnPlane.toFixed(1)} hours`
-//                 break;
-//             default:
-//                 break;
+function callCountry(searchValue) {
+    box.innerHTML = '';
+
+    let found = false;
+    const startIndex = (currentPage - 1) * itemsPerpage;
+    const endIdex = Math.min(startIndex + itemsPerpage, totalItems)
+    const card_wrapper = document.createElement('div')
+    card_wrapper.classList.add('card-wrapper')
+
+
+
+    newData.slice(startIndex, endIdex).forEach((country) => {
+        if (country !== null && country.currencies !== undefined) {
+            const countryName = country.name.common.toLowerCase();
+            const searchData = searchValue.toLowerCase();
+            const countryCurrencies = Object.keys(country.currencies)
+
+            if (countryName.includes(searchData)) {
+                const card = document.createElement('div')
+                card.classList.add('card')
+
+                const image = document.createElement('img')
+                image.src = country.flags.png
+                image.alt = countryName
+
+                const card_content = document.createElement('div')
+                card_content.classList.add('card__content')
+
+                const title = document.createElement('h2')
+                title.textContent = countryName
+
+                const population = document.createElement('h3')
+                population.textContent = `Population: ${country.population.toLocaleString()}`
+
+                const region = document.createElement('h3')
+                region.textContent = `Region: ${country.region}`
+
+                const capital = document.createElement('h3')
+                capital.textContent = `Capital: ${country.capital}`
+
+                const currencies = document.createElement('h3')
+                currencies.textContent = `Currencies: ${countryCurrencies}`
+
+                card.appendChild(image)
+                card_content.appendChild(title)
+                card_content.appendChild(population)
+                card_content.appendChild(region)
+                card_content.appendChild(capital)
+                card_content.appendChild(currencies)
+                card_wrapper.appendChild(card)
+                card.appendChild(card_content)
+                box.appendChild(card_wrapper)
+
+                found = true;
+            }
+        }
+    })
+
+    window.scrollTo(0, 0)
+
+    loading.style.display = 'none'
+
+    if (!found) {
+        notFoundMassage.style.display = 'block'
+    } else {
+        loading.style.display = 'none'
+        notFoundMassage.style.display = 'none'
+    }
+}
+
+const pagination = document.querySelector('.pagination')
+
+function generatePaninationLinks() {
+    pagination.innerHTML = '';
+    const totalPage = Math.round(totalItems / itemsPerpage)
+
+    newData.slice(0, totalPage).forEach((_, i) => {
+        const pageLink = document.createElement('li')
+        pageLink.textContent = i + 1;
+
+        pagination.appendChild(pageLink)
+
+        pageLink.addEventListener('click', () => {
+            currentPage = i + 1;
+            callCountry('');
+            updatePaginationLink()
+            if (index + 1 === currentPage) {
+                pageLink.classList.add('active');
+            }
+        })
+
+
+    })
+}
+
+function updatePaginationLink() {
+    const pageLinks = pagination.querySelectorAll('li')
+    pageLinks.forEach((link, index) => {
+        if (index + 1 === currentPage) {
+            link.classList.add('active')
+        } else {
+            link.classList.remove('active')
+        }
+    })
+}
+
+
+
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const searchValue = input.value.trim();
+    callCountry(searchValue)
+    form.reset();
+})
+
+
+// function generatePaginationLinks() {
+//     pagination.innerHTML = '';
+//     const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+//     console.log(newNumber(4));
+//     myData.slice(0, totalPages).forEach((_, index) => {
+//         const pageLink = document.createElement('li');
+//         pageLink.textContent = index + 1;
+
+//         pageLink.addEventListener('click', () => {
+//             currentPage = index + 1;
+//             updateCard(searchInput.value);
+//             updatePaginationLink();
+//         });
+//         if (index + 1 === currentPage) {
+//             pageLink.classList.add('active');
 //         }
 
+//         pagination.appendChild(pageLink);
 //     })
 
-//     form.reset()
+// }
+
+// function updatePaginationLink() {
+//     const pageLinks = document.querySelectorAll('li');
+//     pageLinks.forEach((link, index) => {
+//         if (index + 1 === currentPage) {
+//             link.classList.add('active')
+//         } else {
+//             link.classList.remove('active')
+//         }
+//     })
+// }
+
+// input.addEventListener('input', () => {
+//     const searchValue = input.value.trim();
+//     callCountry(searchValue)
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Эксперемантальный код
+
+// form.addEventListener('submit', async(e) => {
+//     e.preventDefault();
+//     const inputvalue = Object.fromEntries(new FormData(e.target))
+
+//     const response = await fetch(`https://restcountries.com/v3.1/name/${inputvalue.name}`);
+
+//     if (response.ok) {
+//         const data = await response.json();
+//         callCountry(data)
+//     } else {
+//         alert('NOT FOUND!')
+//     }
+
+
+
 // })
-
-
-// const celsiusInp = document.getElementById('toFarengeyt'),
-//     farengeytForm = document.querySelector('.faren_form'),
-//     h2_2 = document.querySelector('.count')
-
-// farengeytForm.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     const C = celsiusInp.value.trim();
-//     const calc = (C * (9 / 5)) + 32;
-
-//     h2_2.textContent = `${calc}°F`
-// })
-
-const box = document.querySelector('.clock')
-
-window.onload = function() {
-    window.setInterval(function() {
-        const date = new Date();
-
-        let hours = date.getHours();
-        let minutes = date.getMinutes();
-        let second = date.getSeconds();
-
-        if (hours < 10) { hours = '0' + hours }
-        if (minutes < 10) { minutes = '0' + minutes }
-        if (second < 10) { second = '0' + second }
-
-        const clock = hours + ':' + minutes + ':' + second;
-
-        box.textContent = clock;
-    }, 1000);
-}
