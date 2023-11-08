21d7a57a8362989295f49c67d301c6fbded84502
Baxtiyorr <hamrokulovbahtier46@gmail.com>
2023-11-08 23:22:54 +0500

    movies API all works

--- js/main.js
+++ js/main.js
@@ -1,89 +1,108 @@
-const box = document.querySelector('.card_container')
-const loading = document.querySelector('.loading')
+const box = document.querySelector('.box')
 const form = document.querySelector('.form')
-const input = document.querySelector('.search_input')
+const input = document.querySelector('.input')
+const loading = document.querySelector('.loading')
 const not_found_massage = document.querySelector('.not_found')
+const search_btn = document.querySelector('.search_btn')
+const select = document.querySelector('.select')
 
-
-
-const github_api_url = 'https://api.github.com/users?/per_page-80';
-const token = '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'
-
-const data = fetch(github_api_url, {
+const options = {
     method: 'GET',
     headers: {
-        'Authorization': `token ${token} `,
-        'X-GitHub-Api-Version': '2022-11-28'
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ODU3MmIwZWU3OWE3ZWE1ZDdjNTYzZTVhNjhkMzljZiIsInN1YiI6IjY1NGI5YzY3NjdiNjEzMDEwMmUxYjFjMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MI-1wzheKLHaaSIDKGu2LGyu7e8dtG31x9LdIwCuAfk'
     }
-})
+};
 
+const movie_url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1'
 
-async function get_data(url) {
+const searchUrl = 'https://api.themoviedb.org/3/search/movie?query='
+
+async function get_movies(url) {
     try {
-        const response = fetch(url, {
-            method: 'GET',
-            headers: {
-                'Authorization': `token ${token} `,
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        })
-        const data = (await response).json();
-        return data
+        const response = await fetch(url, options);
+        if (response.status !== 200) {
+            console.log('error response');
+        }
+        const movies = await response.json();
+        return movies
     } catch (error) {
-        console.log(error);
+        console.error(error)
     }
 }
 
+const img_url = 'https://image.tmdb.org/t/p/w500'
 
-form.addEventListener('submit', async(e) => {
-    e.preventDefault()
-    const search_value = input.value.trim().toLowerCase()
-
-    const users = await get_data(`https://api.github.com/search/code?q=${search_value}`)
-
-    // users_render(users)
-
-})
-
-
-function users_render(users) {
+function render_movie(movies) {
+    box.innerHTML = '';
     let found = false;
     const card_wrapper = document.createElement('div')
     card_wrapper.classList.add('card-wrapper')
-    users.forEach((user) => {
+    movies.forEach((movie) => {
+        const { backdrop_path, overview, vote_average, original_title } = movie
+
         const card = document.createElement('div')
         card.classList.add('card')
 
         const image = document.createElement('img')
-        image.src = user.avatar_url
-        image.alt = user.login
+        image.src = img_url + backdrop_path
 
+        card.appendChild(image)
         const card_content = document.createElement('div')
         card_content.classList.add('card__content')
 
-        const user_log = document.createElement('h2')
-        user_log.textContent = `Login: ${user.login}`
+        const title = document.createElement('h2')
+        title.textContent = original_title
 
-        const user_id = document.createElement('h4')
-        user_id.classList.add('user_id')
-        user_id.textContent = `User ID: ${user.id}`
-        const status = document.createElement('h3')
-        status.textContent = `Status: ${user.type}`
+        const desc = document.createElement('p')
+        const trimmed_desc = overview.substring(0, 49)
+        desc.textContent = trimmed_desc
+
+        const read_more = document.createElement('button')
+        read_more.classList.add('read_more_btn')
+        read_more.textContent = `readmore...`
+        read_more.addEventListener('click', () => {
+            desc.textContent = overview
+            read_more.style.display = 'none'
+            close.style.display = 'block'
+        })
 
 
-        card.appendChild(user_id)
-        card.appendChild(image)
-        card_content.appendChild(user_log)
-        card_content.appendChild(status)
-        card_wrapper.appendChild(card)
+
+        const close = document.createElement('button')
+        close.classList.add('close')
+        close.textContent = 'close'
+        close.addEventListener('click', () => {
+            desc.textContent = trimmed_desc
+            read_more.style.display = 'block'
+            close.style.display = 'none'
+        })
+
+
+        const votes = document.createElement('span')
+        votes.textContent = vote_average
+
+        if (vote_average <= 4) {
+            votes.classList.add('span-color-red')
+        } else if (vote_average >= 5 && vote_average <= 7.5) {
+            votes.classList.add('span-color-yellow')
+        } else if (vote_average > 7.5) {
+            votes.classList.add('span-color-green')
+        }
+
+        card_content.appendChild(title)
+        card_content.appendChild(desc)
+        card_content.appendChild(read_more)
+        card_content.appendChild(close)
+        card_content.appendChild(votes)
         card.appendChild(card_content)
+
+        card_wrapper.appendChild(card)
         box.appendChild(card_wrapper)
-
         found = true;
-    })
-
+    });
+    window.scrollTo(0, 0)
     loading.style.display = 'none'
-
     if (!found) {
         not_found_massage.style.display = 'block'
     } else {
@@ -92,23 +111,39 @@ function users_render(users) {
     }
 }
 
-get_data(github_api_url).then((data) => users_render(data))
+
+get_movies(movie_url).then((movie) => render_movie(movie.results))
 
 
+form.addEventListener('submit', async(event) => {
+    event.preventDefault();
+    const searchValue = input.value.trim().toLowerCase();
+    const movies = await get_movies(`${searchUrl}${searchValue}`);
+    render_movie(movies.results);
+    form.reset()
+});
+
+select.addEventListener('change', async() => {
+    const selectedValue = select.value.trim().toLowerCase();
+    const movie = await get_movies(`${movie_url}&sort_by=${selectedValue}`)
+    render_movie(movie.results)
+})
 
 
+const pages = document.querySelectorAll('ul li span')
+pagination(pages)
 
-
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     const searchValue = input.value.trim();
-//     users_render(searchValue)
-//     form.reset();
-// })
-
-
-// input.addEventListener('input', () => {
-//     const searchValue = input.value.trim();
-//     callCountry(searchValue)
-// });
+function pagination(pages) {
+    pages.forEach((page, index) => {
+        page.addEventListener('click', async() => {
+            const activePage = document.querySelector('.active')
+            if (activePage) {
+                activePage.classList.remove('active')
+            }
+            page.classList.add('active')
+            const page_content = page.textContent
+            const movies = await get_movies(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page_content}`)
+            render_movie(movies.results)
+        })
+    })
+}
